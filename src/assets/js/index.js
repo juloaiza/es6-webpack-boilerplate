8161d9a463c698edd2f49b750793c7296cbf76ae
Julian Loaiza <julian.loaiza1@t-mobile.com>
2020-03-12 15:35:25 -0700

    update on index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,8 @@
 import "../css/style.scss";
 import User, { printName as printUserName, printAge } from "./user.js";
 import $ from "../../vendors/jquery/jquery-3.4.1.min.js";
+//import * as Module from './modules/module.js';  //import each module's features inside a module object
+
 const user = new User("Peter", 10);
 
 console.log(user);
